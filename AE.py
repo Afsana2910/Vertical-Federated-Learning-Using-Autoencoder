2e581b98fab6bb61533cd940af024107ef6ddaba
Afsana Khan <58783192+Afsana2910@users.noreply.github.com>
2022-03-09 11:56:48 +0100

    Update AE.py

--- AE.py
+++ AE.py
@@ -2,83 +2,38 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import math
+torch.manual_seed(420)
 
-#OverComplete AutoEncoder
-class AE(nn.Module):
-    def __init__(self, input_size):
-        super().__init__()
-
-        #Encoder
-        self.input_layer = nn.Linear(
-            in_features=input_size, out_features=64
-        )
-   
-
-        self.hidden_layer1 = nn.Linear(
-            in_features=64, out_features=128
-        )
-    
-        #Decoder
-        self.hidden_layer2 = nn.Linear(
-            in_features=128, out_features=64
-        )
-
-
-        self.output_layer = nn.Linear(
-            in_features=64, out_features=input_size
-        )
-      
+class autoencoder(nn.Module):
+    def __init__(self, input_size, rate):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, int(input_size*rate)),
+            nn.ReLU(True),
+            nn.Linear(int(input_size*rate), int(input_size*rate)),
+            nn.ReLU(True),
+            nn.Linear(int(input_size*rate), int(input_size*rate)),
+            nn.ReLU(True)
+            )
+            # nn.Linear(64, 128),
+            # nn.ReLU(True))
+        self.decoder = nn.Sequential(
+            nn.Linear(int(input_size*rate), int(input_size*rate)),
+            nn.ReLU(True),
+            nn.Linear(int(input_size*rate), int(input_size*rate)),
+            nn.ReLU(True),
+            nn.Linear(int(input_size*rate), input_size),
+            nn.ReLU(True)
+            )
+            # nn.Linear(64, input_size),
+            # nn.ReLU(True))
         self.double()
-  
 
-    def forward(self, features):
-        
-        x_hat=self.input_layer(features)
-        x_hat=nn.functional.relu(x_hat)
-        x_hat=self.hidden_layer1(x_hat)
-        x_hat=nn.functional.relu(x_hat)
+    def forward(self, x):
+        x_hat = self.encoder(x)
         latent_data = x_hat.detach().numpy()
-        x_hat=self.hidden_layer2(x_hat)
-        x_hat=nn.functional.relu(x_hat)
-        x_hat=self.output_layer(x_hat)
-        x_hat=nn.functional.relu(x_hat)
+        x_hat = self.decoder(x_hat)
         return x_hat, latent_data
-    
-    
-#UnderComplete AutoEncoder
-# class AE(nn.Module):
-#     def __init__(self, input_size):
-#         super().__init__()
-
-#         ##Encoder
-#         self.input_layer = nn.Linear(
-#             in_features=input_size, out_features=(int(input_size/2))
-#         )
-   
-
-#         self.hidden_layer1 = nn.Linear(
-#             in_features=(int(input_size/2)), out_features=(int(input_size/2))
-#         )
-    
-#         ##Decoder
-
-#         self.output_layer = nn.Linear(
-#             in_features=(int(input_size/2)), out_features=input_size
-#         )
-      
-#         self.double()
-  
-
-#     def forward(self, features):
-        
-#         x_hat=self.input_layer(features)
-#         x_hat=nn.functional.relu(x_hat)
-#         x_hat=self.hidden_layer1(x_hat)
-#         x_hat=nn.functional.relu(x_hat)
-#         latent_data = x_hat.detach().numpy()
-#         x_hat=self.output_layer(x_hat)
-#         x_hat=nn.functional.relu(x_hat)
-#         return x_hat, latent_data
 
 
 
