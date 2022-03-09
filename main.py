11b9e2c492a7ecb4b8087e9ccfa9de0483c59283
Afsana Khan <58783192+Afsana2910@users.noreply.github.com>
2022-03-09 11:58:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,95 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score,mean_squared_error, mean_absolute_error,accuracy_score,roc_auc_score
-import scipy as sp 
-import redwine_dataset as rd
-sp.random.seed(42) 
-import AE as autoencoder
-import train_autoencoder 
+from sklearn.metrics import accuracy_score,roc_auc_score
+from AE import autoencoder
+import utils 
 from sklearn.preprocessing import StandardScaler
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 import adultincomedataset as ad
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 import numpy as np
 import verticalsplit as vs
-
-
+import diabetesData as data
+from sklearn.linear_model import SGDClassifier
+import utils
+import heartdiseasedataset_split as ht
+import redwine_dataset as rd
+import iotattack
+#Get Training Data for Each Client
 c1 , y = vs.client1_data()
 c2 = vs.client2_data()
 c3 = vs.client3_data()
-model1 = autoencoder.AE(input_size=c1.shape[1])
-model2 = autoencoder.AE(input_size=c2.shape[1])
-model3 = autoencoder.AE(input_size=c3.shape[1])
-
-encode1 = train_autoencoder.Train(torch.tensor(c1,dtype=torch.double), model1)
-encode2 = train_autoencoder.Train(torch.tensor(c2,dtype=torch.double), model2)
-encode3 = train_autoencoder.Train(torch.tensor(c3,dtype=torch.double), model3)
 
 
-c1_latent = encode1.get_latentdata()
-c2_latent = encode2.get_latentdata()
-c3_latent = encode3.get_latentdata()
-
-
-merged = np.hstack((c1_latent,c2_latent,c3_latent))
-
-x_train, x_test, y_train, y_test = train_test_split(merged, y, test_size=0.3, random_state=42)
-
-
-acc = []
-rocauc = []
-for i in range(10):
-
-
-    logr_pipe = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1000,penalty='l1', solver='liblinear'))
-    logr_pipe.fit(x_train,y_train)
-    y_pred = logr_pipe.predict(x_test)
-    acc.append(accuracy_score(y_test, y_pred))
-    rocauc.append(roc_auc_score(y_test, y_pred))
+#Initialize autoencoder class instances for training data of each client
  
-    
-print("Accuracy: ", max(acc))
-print("ROCAUC: ", max(rocauc))
+model1 = autoencoder(input_size=c1.shape[1],rate=0.25)
+model2 = autoencoder(input_size=c2.shape[1],rate=0.25)
+model3 = autoencoder(input_size=c3.shape[1],rate=0.25)
 
+path='C:/Users/P70077043/Documents/Autoencoder/heartDiseaseDataset/75percentcompression/shuffle2/'
+
+train=True
+
+if train:
+    utils.encoder_trainer(model=model1,data=c1,path=path,file_name='c1',percent=0.25,num_epochs=500,lr= 1e-2,batch_size=50)
+    utils.encoder_trainer(model=model2,data=c2,path=path,file_name='c2',percent=0.25,num_epochs=500,lr= 1e-2,batch_size=50)
+    utils.encoder_trainer(model=model3,data=c3,path=path,file_name='c3',percent=0.25,num_epochs=500,lr= 1e-2,batch_size=50)
+    
+
+c1 , y = c1[int(c1.shape[0]*0.25):,:] , y[int(c1.shape[0]*0.25):] 
+c2 = c2[int(c2.shape[0]*0.25):,:]
+c3 = c3[int(c3.shape[0]*0.25):,:] 
+
+#Train data for training model after aggregation 
+c1_train, y_train = c1[:int(c1.shape[0]*0.75),:], y[:int(c1.shape[0]*0.75)] 
+c2_train = c2[:int(c2.shape[0]*0.75),:]
+c3_train = c3[:int(c3.shape[0]*0.75),:]
+
+
+#Test data for testing model after aggregation 
+c1_test, y_test = c1[int(c1.shape[0]*0.75):,:], y[int(c1.shape[0]*0.75):] 
+c2_test = c2[int(c2.shape[0]*0.75):,:]
+c3_test = c3[int(c3.shape[0]*0.75):,:]
+
+model1.load_state_dict(torch.load(path+'c1.pth'))
+_,en_c1=model1(torch.tensor(c1_train,dtype=torch.double))
+
+model2.load_state_dict(torch.load(path+'c2.pth'))
+_,en_c2=model2(torch.tensor(c2_train,dtype=torch.double))
+
+model3.load_state_dict(torch.load(path+'c3.pth'))
+_,en_c3=model3(torch.tensor(c3_train,dtype=torch.double))
+
+model1.load_state_dict(torch.load(path+'c1.pth'))
+_,en_c1_test=model1(torch.tensor(c1_test,dtype=torch.double))
+
+model2.load_state_dict(torch.load(path+'c2.pth'))
+_,en_c2_test=model2(torch.tensor(c2_test,dtype=torch.double))
+
+model3.load_state_dict(torch.load(path+'c3.pth'))
+_,en_c3_test=model3(torch.tensor(c3_test,dtype=torch.double))
+
+
+merged_train = np.hstack((c1_train,en_c2,en_c3))
+merged_test = np.hstack((c1_test,en_c2_test,en_c3_test))
+
+merged_train_encoded = np.hstack((en_c1,en_c2,en_c3))
+merged_test_encoded = np.hstack((en_c1_test,en_c2_test,en_c3_test))
+#logr_pipe = make_pipeline(StandardScaler(),SGDClassifier(max_iter=1000, tol=1e-3)).fit(merged_train,y_train)
+
+logr_pipe2 = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1200)).fit(merged_train_encoded,y_train)
+y_pred2 = logr_pipe2.predict(merged_test_encoded)
+print("Accuracy encoding client 1 data: ", accuracy_score(y_test, y_pred2))
+print("ROC_AUC encoding client 1 data: ", roc_auc_score(y_test, y_pred2))
+
+logr_pipe1 = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1200)).fit(merged_train,y_train)
+y_pred1 = logr_pipe1.predict(merged_test)
+print("Accuracy without encoding client 1 data: ", accuracy_score(y_test, y_pred1))
+print("ROC_AUC without encoding client 1 data: ", roc_auc_score(y_test, y_pred1))
 
